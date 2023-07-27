137af491ff016d87e2038279317001ae7ff6e8cf
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-27 15:19:13 +0530

    added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+let timer = () => {
+    let date = new Date();
+    // console.log(date.toLocaleDateString()); 
+    // console.log(date.toLocaleTimeString())
+    let todayDate = date.toLocaleDateString();
+    let todayTime = date.toLocaleTimeString();
+
+    let p = document.getElementById("timer");
+    p.innerText = `${todayDate}, ${todayTime}`;
+}
+
+timer();
